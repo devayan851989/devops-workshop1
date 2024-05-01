d55b2a3ef93d4bd66d54ab81a0c91aa992c748dd
Devayan Thakur <devayanthakur@gmail.com>
2024-05-01 19:41:47 +0530

    V2-EC2

--- V2-EC2.tf
+++ V2-EC2.tf
@@ -7,33 +7,50 @@ provider "aws" {
   ami           = "ami-013e83f579886baeb"
   instance_type = "t2.micro"
   key_name     = "dpp-keypair"
-  security_groups = [ "allow_tls" ]
+  security_groups = [ "demo-sg" ]
  }
 
- resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+ 
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
   description = "SSH Access"
-  #vpc_id      = aws_vpc.main.id
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    Name = "allow_tls"
+    Name = "ssh-prot"
+
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
 
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  from_port         = 22
-  to_port           = 22
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   from_port         = 22
+#   to_port           = 22
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
 
